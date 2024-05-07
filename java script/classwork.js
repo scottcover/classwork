3780cb32efab27384a4fda93f3724b3bc7e3caca
scottcover <scottcover@outlook.com>
2024-05-06 19:49:17 -0500

    java classwork

---
+++ java script/classwork.js
@@ -0,0 +1,30 @@
+// 1. Positive or Negative
+// Create a program that checks if a number stored in a variable is positive or negative. 
+// Log "The number is positive" if it's greater than zero, otherwise log "The number is negative."
+
+
+let number = 22;
+
+if (number % 2 === 0) {
+    console.log(number + " is even");
+} 
+    else {
+    console.log(number + " is odd");
+}
+
+
+
+// 4. Basic Discount System
+// A shop offers a discount for purchases over $50. 
+// Write a program where the total purchase amount is stored in a variable. 
+// If the amount is over $50, calculate the discount and log it. Otherwise, log "No discount available."
+
+
+let dollars = 85;
+
+if (dollars >= 50 ) {
+    console.log(dollars * .85)
+}
+    else {
+    console.log("No discount available")
+}
